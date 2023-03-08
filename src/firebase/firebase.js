62ba78ab1d2e535484c45e80fcfbe9ff60fd9043
Sara Ahmad <97902937+sara-ahmadd@users.noreply.github.com>
2023-03-08 18:54:34 +0200

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, collection, getDocs } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBmtehaNLd5TSATD27A6r87zGWdVq611Zg",
-  authDomain: "advanced-ecommerce-app-d2bc0.firebaseapp.com",
-  projectId: "advanced-ecommerce-app-d2bc0",
-  storageBucket: "advanced-ecommerce-app-d2bc0.appspot.com",
-  messagingSenderId: "223525391965",
-  appId: "1:223525391965:web:90ccc3b896995002c5c7b4",
-};
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
